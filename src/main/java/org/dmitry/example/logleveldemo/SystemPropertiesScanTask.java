24debc198461fde2511b80ac8fdb39065a5424f7
dmitry <dbufistov@ntent.com>
2022-11-13 22:39:16 +0100

    Small cleanup, still rely on logback variables.

---
+++ src/main/java/org/dmitry/example/logleveldemo/SystemPropertiesScanTask.java
@@ -0,0 +1,30 @@
+package org.dmitry.example.logleveldemo;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SystemPropertiesScanTask extends Properties {
+  private static final Logger log = LoggerFactory.getLogger(SystemPropertiesScanTask.class);
+  @Value("${spring.config.location}")
+  private String propertyFile;
+
+  @Scheduled(fixedDelay = 1000)
+  void loadProperties() {
+    try {
+      load(new FileReader(propertyFile));
+      log.debug("Loaded properties file with {} properties", entrySet().size());
+      for (var entries: entrySet()) {
+        System.setProperty((String) entries.getKey(), (String) entries.getValue());
+      }
+    } catch (Exception exception) {
+      log.warn("Failed to load properties from file: {}", propertyFile);
+    }
+  }
+}
--- src/main/java/org/dmitry/example/logleveldemo/Task.java
+++ src/main/java/org/dmitry/example/logleveldemo/Task.java
@@ -1,11 +1,8 @@
 package org.dmitry.example.logleveldemo;
 
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,26 +10,10 @@ import org.springframework.stereotype.Component;
 public class Task extends Properties {
 
   private static final Logger log = LoggerFactory.getLogger(Task.class);
-  @Value("${spring.config.location}")
-  private String propertyFile;
 
   @Scheduled(fixedDelay = 1000)
   void run() {
     log.info("Logger level is >= info");
     log.debug("Logger level is >= debug");
-  }
-
-  // @Scheduled(fixedDelay = 10_00)
-  void loadProperties() {
-    try {
-      loadFromXML(new FileInputStream(propertyFile));
-      log.debug("Loaded properties file with {} properties", entrySet().size());
-      for (var entries: entrySet()) {
-        log.debug("Setting {} to value {}", entries.getKey(), entries.getValue());
-        System.setProperty((String) entries.getKey(), (String) entries.getValue());
-      }
-    } catch (Exception exception) {
-      log.warn("Failed to load properties from file: {}", propertyFile);
-    }
   }
 }

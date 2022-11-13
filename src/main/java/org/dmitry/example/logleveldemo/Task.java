d7823a3889e73cc32101f1b99414c220d98b679a
dmitry <dbufistov@ntent.com>
2022-11-13 17:58:30 +0100

    Trying to re-configure logger level at runtime.
    Does not work for now.

--- src/main/java/org/dmitry/example/logleveldemo/Task.java
+++ src/main/java/org/dmitry/example/logleveldemo/Task.java
@@ -1,14 +1,19 @@
 package org.dmitry.example.logleveldemo;
 
+import java.io.FileReader;
+import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Task {
+public class Task extends Properties {
 
   private static final Logger log = LoggerFactory.getLogger(Task.class);
+  @Value("${spring.config.location}")
+  private String propertyFile;
 
   @Scheduled(fixedDelay = 1000)
   void run() {
@@ -16,4 +21,17 @@ public class Task {
     log.debug("Logger level is >= debug");
   }
 
+  @Scheduled(fixedDelay = 10_00)
+  void loadProperties() {
+    try {
+      load(new FileReader(propertyFile));
+      log.debug("Loaded properties file with {} properties", entrySet().size());
+      for (var entries: entrySet()) {
+        log.debug("Setting {} to value {}", entries.getKey(), entries.getValue());
+        System.setProperty((String) entries.getKey(), (String) entries.getValue());
+      }
+    } catch (Exception exception) {
+      log.warn("Failed to load properties from file: {}", propertyFile);
+    }
+  }
 }
